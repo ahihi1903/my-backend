aa99c028d47e2cd840e0530cbfa7c09fe7672dbc
Chu_Tuan_Kiet <abibihkt@gmail.com>
2025-11-19 02:00:59 +0700

    update index.js and file .env.example

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// index.js
+// // index.js
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+
+// const app = express();
+// app.use(express.json());
+
+// app.get("/", (req, res) => res.send("API is running"));
+
+// const PORT = process.env.PORT || 3000;
+// mongoose
+//   .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/test")
+//   .then(() => {
+//     console.log("MongoDB connected");
+//     app.listen(PORT, () => console.log("Server running on port " + PORT));
+//   })
+//   .catch((err) => console.error("Mongo error:", err));
+
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -6,13 +24,15 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("API is running"));
-
-const PORT = process.env.PORT || 3000;
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/test")
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log("Server running on port " + PORT));
-  })
-  .catch((err) => console.error("Mongo error:", err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
